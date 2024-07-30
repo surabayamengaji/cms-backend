63a08c4f078b8831175bf3d300b63101b8292598
Ardhika Ryzha Nurmawan <ardhikarn@gmail.com>
2024-07-30 14:55:28 +0700

    chore: migrate and setup directory

---
+++ prisma/migrations/20240730045732_create_users_table/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - The primary key for the `users` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `id` on the `users` table. All the data in the column will be lost.
+  - Made the column `name` on table `users` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP CONSTRAINT "users_pkey",
+DROP COLUMN "id",
+ALTER COLUMN "name" SET NOT NULL,
+ADD CONSTRAINT "users_pkey" PRIMARY KEY ("username");
